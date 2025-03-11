6ccb2918cdce9bbf7cb92d5eecbd6e9116e418b9
Vitor Lima <vitorsawada2002@gmail.com>
2025-03-10 23:49:27 -0300

    :recycle: refactor: updateTotalPages e handleSearch, atualizados para seguir a convenção de object calisthenics

--- src/composables/useLoadRepositories.ts
+++ src/composables/useLoadRepositories.ts
@@ -1,27 +1,27 @@
-import { ref } from 'vue';
-import useAxios from './useAxios';
-import type { Repository } from '@/types/repository';
+import { ref } from "vue";
+import useAxios from "./useAxios";
+import type { Repository } from "@/types/repository";
 
 const { get } = useAxios();
 
-type GithubVisibility = 'all' | 'public' | 'private';
+type GithubVisibility = "all" | "public" | "private";
 
 const repositories = ref<Repository[]>([]);
 const currentPage = ref<number>(1);
 const itemsPerPage = 8; // Pode testar com outro valor :)
-const totalPages = ref<number>(1); 
-const searchName = ref<string>('');
-const searchVisibility = ref<GithubVisibility>('all');
+const totalPages = ref<number>(1);
+const searchName = ref<string>("");
+const searchVisibility = ref<GithubVisibility>("all");
 
 const extractTotalPagesFromLinkHeader = (linkHeader: string): number => {
-  const links = linkHeader.split(', ');
+  const links = linkHeader.split(", ");
   let lastParamPage = 0;
 
   links.forEach((link) => {
     if (link.includes('rel="last"')) {
-      const url = link.split('; ')[0].slice(1, -1);
-      const urlParams = new URLSearchParams(url.split('?')[1]);
-      const lastPage = urlParams.get('page');
+      const url = link.split("; ")[0].slice(1, -1);
+      const urlParams = new URLSearchParams(url.split("?")[1]);
+      const lastPage = urlParams.get("page");
       lastParamPage = lastPage ? parseInt(lastPage, 10) : 0;
     }
   });
@@ -32,32 +32,31 @@ const extractTotalPagesFromLinkHeader = (linkHeader: string): number => {
 const updateTotalPages = (linkHeader: string | undefined): void => {
   if (!linkHeader) {
     totalPages.value = 1;
-  } else {
-    const extractedPages = extractTotalPagesFromLinkHeader(linkHeader);
-          if (extractedPages > 0) {
-        totalPages.value = extractedPages;
-      }
+    return;
+  }
+  const extractedPages = extractTotalPagesFromLinkHeader(linkHeader);
+  if (extractedPages > 0) {
+    totalPages.value = extractedPages;
   }
 };
 
 const loadRepositories = async (page: number): Promise<void> => {
   try {
-    const { data, headers } = await get<Repository[]>('/user/repos', {
+    const { data, headers } = await get<Repository[]>("/user/repos", {
       per_page: itemsPerPage,
       page: page,
-      sort: 'created',
+      sort: "created",
       visibility: searchVisibility.value,
-      affiliation: 'owner',
+      affiliation: "owner",
     });
 
-    
     repositories.value = data;
-   
+
     updateTotalPages(headers.link);
 
     currentPage.value = page;
   } catch (error) {
-    console.error('Nao conseguiu dar fetch nos repositorios:', error);
+    console.error("Nao conseguiu dar fetch nos repositorios:", error);
   }
 };
 
@@ -66,18 +65,21 @@ const searchRepositories = async (page: number): Promise<void> => {
     const params: Record<string, any> = {
       per_page: itemsPerPage,
       page: page,
-      q: `user:${import.meta.env.VITE_USER_NAME} ${searchName.value} in:name`,  
+      q: `user:${import.meta.env.VITE_USER_NAME} ${searchName.value} in:name`,
     };
 
-    const { data, headers } = await get<{ items: Repository[] }>('/search/repositories', params);
-    
+    const { data, headers } = await get<{ items: Repository[] }>(
+      "/search/repositories",
+      params
+    );
+
     repositories.value = data.items;
 
     updateTotalPages(headers.link);
 
     currentPage.value = page;
   } catch (error) {
-    console.error('Nao conseguiu dar fetch nos repositorios:', error);
+    console.error("Nao conseguiu dar fetch nos repositorios:", error);
   }
 };
 
@@ -89,9 +91,10 @@ const handleVisibility = (visibility: GithubVisibility): void => {
 const handleSearch = (page: number = 1): void => {
   if (searchName.value) {
     searchRepositories(page);
-  } else {
-    loadRepositories(page);
-  }
+    return;
+  } 
+  loadRepositories(page);
+ 
 };
 
 export {
